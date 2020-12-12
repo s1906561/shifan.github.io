86e8c3772087cc2f226838c1a6a0c592c52de1d1
Shifan <59900961+s1906561@users.noreply.github.com>
2020-12-12 21:23:32 +0800

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h2');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Hi Shifan, I am ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Hi Shifan, I am ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
